68e1f2c1685754b350b2a80441ff570ae729bd5d
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2023-07-01 17:39:42 -0400

    implement fancy-triangle

--- src/sketches/fancy-triangle/FancyTriangle.svelte
+++ src/sketches/fancy-triangle/FancyTriangle.svelte
@@ -4,7 +4,7 @@
 
   let canvas: HTMLCanvasElement;
 
-  async function main() {
+  async function init() {
     // Check to see if user's browser supports WebGPU
     const adapter = await navigator.gpu?.requestAdapter();
     const device = await adapter?.requestDevice();
@@ -56,6 +56,11 @@
     };
 
     function render() {
+      // Get the current texture from the canvas context and
+      // set it as the texture to render to.
+      renderPassDescriptor.colorAttachments[0].view = context
+        .getCurrentTexture()
+        .createView();
       const encoder = device.createCommandEncoder({ label: "render encoder" });
       const pass = encoder.beginRenderPass(renderPassDescriptor);
       pass.setPipeline(renderPipeline);
@@ -66,17 +71,38 @@
       device.queue.submit([commandBuffer]);
     }
 
+    const observer = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        const canvas = entry.target as HTMLCanvasElement;
+        const width = entry.contentBoxSize[0].inlineSize;
+        const height = entry.contentBoxSize[0].blockSize;
+        canvas.width = Math.max(
+          1,
+          Math.min(width, device.limits.maxTextureDimension2D)
+        );
+        canvas.height = Math.max(
+          1,
+          Math.min(height, device.limits.maxTextureDimension2D)
+        );
+
+        render();
+      }
+    });
+
     render();
+    observer.observe(canvas);
   }
 
-  onMount(main);
+  onMount(() => {
+    init();
+  });
 </script>
 
 <canvas bind:this={canvas} />
 
 <style>
   canvas {
-    width: 600px;
-    height: 600px;
+    width: 100%;
+    height: 100%;
   }
 </style>
