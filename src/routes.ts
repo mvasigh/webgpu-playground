4219a399d44cebc000a90ac9d8018f02e7cc2da0
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2023-07-16 18:29:33 -0400

    reorganize sketches into subdirectories

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
 import type { ComponentType } from "svelte";
 
-import RedTriangle from "./sketches/red-triangle/RedTriangle.svelte";
-import BasicCompute from "./sketches/basic-compute/BasicCompute.svelte";
-import FancyTriangle from "./sketches/fancy-triangle/FancyTriangle.svelte";
-import Uniforms from "./sketches/uniforms/Uniforms.svelte";
+import RedTriangle from "./sketches/basic/red-triangle/RedTriangle.svelte";
+import BasicCompute from "./sketches/compute/basic-compute/BasicCompute.svelte";
+import FancyTriangle from "./sketches/basic/fancy-triangle/FancyTriangle.svelte";
+import Uniforms from "./sketches/basic/uniforms/Uniforms.svelte";
 
 interface Route {
   href: string;
@@ -18,7 +18,7 @@ interface RouteCategory {
 
 export const ROUTE_CATEGORIES: RouteCategory[] = [
   {
-    label: "Sketches",
+    label: "Basic",
     routes: [
       {
         href: "/red-triangle",
